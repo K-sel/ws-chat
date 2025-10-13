6203279833202144cbd98f0ef2ed3056e98cd4fd
Vince <jpinard@bluewin.ch>
2025-10-13 14:28:52 +0200

    Nodemon + Server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -1,8 +1,6 @@
 import { WSServerPubSub } from 'wsmini';
 
 const wsServer = new WSServerPubSub({
-  // Set your server options, e.g., port, allowed origins, max number of clients, ...
-  // See the server documentation for the complete list of options
   port: 8080,
 });
 
